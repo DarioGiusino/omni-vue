452b40782b2b67d89359a3c4663a7df5fd6eb571
DarioGiusino <dario.giusino@gmail.com>
2023-06-11 17:23:30 +0200

    initial scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: "OmniTest",
+  components: { AppHeader, AppMain, AppFooter }
+}
 </script>
 
 <template>
-  <h1>hello world!</h1>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 <style lang="scss" scoped></style>
