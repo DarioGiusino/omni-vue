377e47890b369cf785755b38ab240bdbd831ba95
DarioGiusino <dario.giusino@gmail.com>
2023-06-12 23:34:13 +0200

    minor fix

--- src/components/main/PmiSection.vue
+++ src/components/main/PmiSection.vue
@@ -27,11 +27,11 @@ export default {
         </div>
 
         <!-- ? < 992px (top little cards + carousel) -->
+        <!-- little cards -->
         <div class="my-5 d-flex">
             <base-simple-card v-for="card in pmiBaseCards" :key="card.title" :image="card.image" :title="card.title"
                 :text="card.text" class="fb-25">
             </base-simple-card>
-
         </div>
 
         <!-- carousel -->
